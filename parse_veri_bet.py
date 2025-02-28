cad4881585f5204a1f57dae7707589f70f141eb8
Arthur <arthur.inacio3@gmail.com>
2025-02-28 13:14:40 -0300

    feat[bet]: improving code performance & parsing information

--- parse_veri_bet.py
+++ parse_veri_bet.py
@@ -1,7 +1,8 @@
-from datetime import datetime, timezone
+from datetime import datetime
+import pytz
 import re
 import asyncio
-from zoneinfo import ZoneInfo
+import json
 from bs4 import BeautifulSoup
 from playwright.async_api import async_playwright, TimeoutError
 from dataclasses import dataclass
@@ -23,7 +24,7 @@ class Item:
     async def playwright_start(self) -> None:
         # Playwright inicialization
         self.playwright = await async_playwright().start()
-        self.browser = await self.playwright.chromium.launch(headless=False)
+        self.browser = await self.playwright.chromium.launch(headless=True)
         self.context = await self.browser.new_context()
         self.page = await self.context.new_page()
         self.page.set_default_timeout(600000)
@@ -35,7 +36,6 @@ class Item:
         await self.browser.close()
     
     async def _login_access(self) -> list:
-        listagem = []
         await self.page.goto("https://veri.bet/simulator", timeout=50000)
 
         # Access to simulator button
@@ -50,60 +50,86 @@ class Item:
         html = await self.page.content()
         soup = BeautifulSoup(html, "html.parser")
         tables = soup.find_all("table", {"style": "margin-top: 12px; margin-bottom: 15px;"})
+        await self._table_extraction(tables)
 
+    async def _table_extraction(self, tables) -> list:
+        listin_information = []
         for table in tables:
             lines = table.find_all("tr")
             league = re.search(r"sport=(.*?)\"", str(table), re.S).group(1)
             teams = re.findall(r"betting-trends.*?font-size:\s\.\d*rem\;\">(.*?)<", str(table), re.S)
             
             # Date conversion to timestamp
-            game_date = re.search(r"(\d:\d*\s.*?\))", str(table), re.S).group(1)
-           
-            hour = re.search(r"(\d*\:\d*\s\w{2})", str(game_date), re.S)[1]
-            date = re.search(r"(\d*\/\d*\/\d{4})", str(game_date), re.S)[1].replace("0:00 AM", "12:00 AM") 
-        
-            # Converting to date time
-            date = datetime.strptime(f'{hour} {date}', "%I:%M %p %m/%d/%Y").replace(tzinfo=ZoneInfo("America/New_York"))
-
-            # Converting to UTC
-            dt_utc = date.astimezone(ZoneInfo("UTC"))
-
-            # Parsin to ISO
-            date_iso = dt_utc.date().isoformat()
+            try:
+                game_date = re.search(r"(\d:\d*\s.*?\))", str(table), re.S).group(1)
+                iso_format = await self._timezone_ajust(game_date)
+            except(TypeError, AttributeError, ValueError):
+                continue
 
             for line in lines[1:]:
                 span = line.find_all("span")
                 try:
+                    side = span[0].text.strip()
+                    if re.search(r"DRAW", str(table), re.S):
+                        side = "DRAW"
                     spread = re.search(r"(.*?)\s*\(", span[2].text.strip(), re.S).group(1)
+
+                    if not spread or spread == "0.00":
+                        line_type = "moneyline"
+                    elif re.search(r"(\d+\.?\d*)", str(spread), re.S):
+                        line_type = "over/under"
+                    elif re.search(r"([-+]?\d+\.?\d*)", str(spread), re.S):
+                        line_type = "spread"
+
                     item = Item(
                         sport_league=league,
-                        event_date_utc=date_iso,
+                        event_date_utc=iso_format,
                         team1=teams[0],
                         team2=teams[1],
                         pitcher="",
                         period="FULL GAME",
-                        line_type="moneyline",
+                        line_type=line_type,
                         price=span[1].text.strip(),
-                        side=span[0].text.strip(),
-                        team=span[0].text.strip(),
+                        side=side,
+                        team=side,
                         spread=float(spread),
                     )
-                    print(item)
-                    print('==========')
                 except (IndexError, AttributeError):
                     continue
-                listagem.append(item)
-        return listagem
-            
+                break
+        return listin_information
+
+    async def _timezone_ajust(self, date: str) -> str:
+        hour = re.search(r"(\d*\:\d*\s\w{2})", str(date), re.S)[1]
+        date_reg = re.search(r"(\d*\/\d*\/\d{4})", str(date), re.S)[1] 
+
+        # Criar um datetime a partir da string
+        et_tz = pytz.timezone("America/New_York")  # ET (Eastern Time)
+        utc_tz = pytz.utc  # UTC
+
+        # Converter para datetime com timezone
+        event_datetime = datetime.strptime(f"{date_reg} {hour}", "%m/%d/%Y %I:%M %p")
+        event_datetime = et_tz.localize(event_datetime)  # Definir como horário de ET
+
+        # Converter para UTC
+        event_datetime_utc = event_datetime.astimezone(utc_tz)
+
+        # Formatar no padrão ISO 8601
+        iso_format = event_datetime_utc.strftime("%Y-%m-%dT%H:%M:%S%z")
+        iso_format = iso_format[:-2] + ":" + iso_format[-2:]
+        return iso_format
+
 async def main() -> None:
     item = Item()
     await item.playwright_start()
     for _ in range(3):
         try:
-            await item._login_access()
+            data = await item._login_access()
         except TimeoutError:
             continue
+        break
     await item.playwright_finish()
+    print(json.dumps(data, indent=4))
 
 if __name__ == "__main__":
     asyncio.run(main())
